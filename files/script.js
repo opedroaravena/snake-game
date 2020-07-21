472010485deb8344b4657c196101fa0cf1738ff7
Pedro Henrique Aravena Da Costa <pedro_aravena@churchofjesuschrist.org>
2020-07-21 11:13:45 -0300

    Using css and adding audio background

--- files/script.js
+++ files/script.js
@@ -1,3 +1,6 @@
+
+
+
 let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
 let box = 32;
@@ -46,6 +49,14 @@ function iniciarJogo(){
   if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
   if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
   if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
+
+  for(i = 1; i < snake.length; i++){
+      if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+          clearInterval(jogo);
+          alert("GAME OVER D: ");
+          location.reload();
+      }
+  }
 
 
   criarBg();

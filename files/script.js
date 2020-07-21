3ae9333df9545b8389f29522b8d1ff84c125afde
Pedro Henrique Aravena Da Costa <pedro_aravena@churchofjesuschrist.org>
2020-07-21 10:21:13 -0300

    Drawing food

--- files/script.js
+++ files/script.js
@@ -7,6 +7,10 @@ snake[0] = {
   y: 8 * box
 }
 let direction = "right";
+let food = {
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 1) * box
+}
 
 function criarBg() {
   context.fillStyle = "lightblue";
@@ -20,9 +24,33 @@ function criarCobrinha(){
     }
 }
 
+function drawFood(){
+  context.fillStyle = "red";
+  context.fillRect(food.x, food.y, box, box);
+}
+
+
+
+document.addEventListener('keydown', update);
+
+function update (event){
+    if(event.keyCode == 37 && direction != "right") direction = "left";
+    if(event.keyCode == 38 && direction != "down") direction = "up";
+    if(event.keyCode == 39 && direction != "left") direction = "right";
+    if(event.keyCode == 40 && direction != "up") direction = "down";
+}
+
+
 function iniciarJogo(){
+  if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
+  if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
+  if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
+  if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
+
+
   criarBg();
   criarCobrinha();
+  drawFood();
 
   let snakeX = snake[0].x;
   let snakeY = snake[0].y;
@@ -32,7 +60,13 @@ function iniciarJogo(){
   if(direction == "up") snakeY -= box;
   if(direction == "down") snakeY += box;
 
-  snake.pop();
+  if(snakeX != food.x || snakeY != food.y){
+      snake.pop();
+  }
+  else{
+    food.x = Math.floor(Math.random() * 15 + 1) * box,
+    food.y = Math.floor(Math.random() * 15 + 1) * box
+  }
 
   let newHead = {
     x: snakeX,
